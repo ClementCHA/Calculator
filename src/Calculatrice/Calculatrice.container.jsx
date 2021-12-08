9ed7c594a1bcbec44be6159ce979adf8f5d15925
Clement CHARLES <clement.charles@origins-digital.com>
2021-12-08 22:41:18 +0100

    Need a method on multiple operation

--- src/Calculatrice/Calculatrice.container.jsx
+++ src/Calculatrice/Calculatrice.container.jsx
@@ -3,68 +3,171 @@ import React, { useState } from "react";
 import CalculatriceView from "./Calculatrice.view";
 
 const CalculatriceContainer = () => {
+  const [firstOperator, setFirstOperator] = useState("");
   const [number, setNumber] = useState("");
-  const [operator, setOperator] = useState(null);
+  const [secondOperator, setSecondOperator] = useState("");
   const [mathOperation, setMathOperation] = useState("");
+  const [expression, setExpression] = useState(null);
+  const [isDecimal, setIsDecimal] = useState(false);
+
+  const myArrayOfOperator = [
+    { key: "/", value: "/" },
+    { key: "+", value: "+" },
+    { key: "-", value: "-" },
+    { key: "clear", value: "CE" },
+    { key: "=", value: "=" },
+    { key: "x", value: "x" },
+    { key: ",", value: "," },
+  ];
+
+  const myArrayOfNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   const OPERATORVALUE = {
-    DIVIDE: "divide",
-    ADD: "add",
-    SUBSTRACTION: "substraction",
+    MULTIPLY: "x",
+    DIVIDE: "/",
+    ADD: "+",
+    SUBSTRACTION: "-",
     CLEAR: "clear",
-    EGAL: "egal",
+    EGAL: "=",
+    COMMA: ",",
   };
 
   const saveNumber = (e) => {
+    if (expression) {
+      setNumber("");
+    }
     setNumber(number.concat(e.target.value));
+    if (mathOperation.length > 0) {
+      setExpression(null);
+      setNumber(number.concat(e.target.value));
+      setSecondOperator(number.concat(e.target.value));
+    }
   };
 
   const operate = (e) => {
     switch (e.target.value) {
+      //! Clear
       case OPERATORVALUE.CLEAR:
         setNumber("");
-        setOperator(null);
+        setFirstOperator("");
+        setSecondOperator("");
+        setExpression("");
         setMathOperation("");
         break;
-      case OPERATORVALUE.ADD:
-        setMathOperation(OPERATORVALUE.ADD);
-        setOperator(number);
+      //! Substraction
+      case OPERATORVALUE.SUBSTRACTION:
+        setFirstOperator(number);
         setNumber("");
+        setExpression(number);
+        setMathOperation(OPERATORVALUE.SUBSTRACTION);
         break;
-      case OPERATORVALUE.EGAL:
-        if (mathOperation === OPERATORVALUE.ADD) {
-          setNumber(parseInt(operator, 10) + parseInt(number, 10));
-        } else if (mathOperation === OPERATORVALUE.DIVIDE) {
-          setNumber(parseInt(operator, 10) / parseInt(number, 10));
-        } else if (mathOperation === OPERATORVALUE.SUBSTRACTION) {
-          setNumber(parseInt(operator, 10) - parseInt(number, 10));
+      //! Divide
+      case OPERATORVALUE.DIVIDE:
+        setFirstOperator(number);
+        setNumber("");
+        setExpression(number);
+        setMathOperation(OPERATORVALUE.DIVIDE);
+        break;
+      //! Multiply
+      case OPERATORVALUE.MULTIPLY:
+        setFirstOperator(number);
+        setNumber("");
+        setExpression(number);
+        setMathOperation(OPERATORVALUE.MULTIPLY);
+        break;
+      //! Comma
+      case OPERATORVALUE.COMMA:
+        if (!isDecimal) {
+          setIsDecimal(true);
+          setNumber(number.concat(","));
         }
-        setOperator(null);
-        setMathOperation("");
+        break;
+      //! Add
+      case OPERATORVALUE.ADD:
+        setFirstOperator(number);
+        setNumber("");
+        setExpression(number);
+        setMathOperation(OPERATORVALUE.ADD);
+        if (mathOperation.length > 0) {
+          setExpression(null);
+          if (mathOperation === OPERATORVALUE.ADD) {
+            setExpression(
+              `${parseInt(firstOperator, 10) + parseInt(secondOperator, 10)}`
+            );
+            setFirstOperator(
+              `${parseInt(firstOperator, 10) + parseInt(secondOperator, 10)}`
+            );
+            setSecondOperator("");
+            setMathOperation(OPERATORVALUE.ADD);
+          }
+        }
         break;
 
-      case OPERATORVALUE.DIVIDE:
-        setMathOperation(OPERATORVALUE.DIVIDE);
-        setOperator(number);
-        setNumber("");
+      //! Si on appui sur égal, on check l'opération et on fais le calcul nécéssaire avec le nombre actuel + le nombre enristré dans le champs principal
+      case OPERATORVALUE.EGAL:
+        if (mathOperation === OPERATORVALUE.ADD) {
+          setNumber(
+            `${parseInt(firstOperator, 10) + parseInt(secondOperator, 10)}`
+          );
+          setFirstOperator("");
+          setMathOperation("");
+          setSecondOperator("");
+          setExpression(null);
+        }
+        if (mathOperation === OPERATORVALUE.DIVIDE) {
+          setNumber(
+            `${parseInt(firstOperator, 10) / parseInt(secondOperator, 10)}`
+          );
+          setFirstOperator("");
+          setMathOperation("");
+          setSecondOperator("");
+          setExpression(null);
+        }
+
+        if (mathOperation === OPERATORVALUE.SUBSTRACTION) {
+          setNumber(
+            `${parseInt(firstOperator, 10) - parseInt(secondOperator, 10)}`
+          );
+          setFirstOperator("");
+          setMathOperation("");
+          setSecondOperator("");
+          setExpression(null);
+        }
+
+        if (mathOperation === OPERATORVALUE.SUBSTRACTION) {
+          setNumber(
+            `${parseInt(firstOperator, 10) - parseInt(secondOperator, 10)}`
+          );
+          setFirstOperator("");
+          setMathOperation("");
+          setSecondOperator("");
+          setExpression(null);
+        }
+
+        if (mathOperation === OPERATORVALUE.MULTIPLY) {
+          setNumber(
+            `${parseInt(firstOperator, 10) * parseInt(secondOperator, 10)}`
+          );
+          setFirstOperator("");
+          setMathOperation("");
+          setSecondOperator("");
+          setExpression(null);
+        }
         break;
-      case OPERATORVALUE.SUBSTRACTION:
-        setMathOperation(OPERATORVALUE.SUBSTRACTION);
-        setOperator(number);
-        setNumber("");
-        break;
+
       default:
     }
   };
-  console.log(operator);
-  console.log(mathOperation);
   // as we had characters, we will need to parseInt value to have int
   return (
     <>
       <CalculatriceView
+        myArrayOfOperator={myArrayOfOperator}
         saveNumber={saveNumber}
         number={number}
         operate={operate}
+        myArrayOfNumber={myArrayOfNumber}
+        expression={expression}
       />
     </>
   );
--- src/Calculatrice/Calculatrice.view.jsx
+++ src/Calculatrice/Calculatrice.view.jsx
@@ -2,28 +2,15 @@ import React from "react";
 
 import styles from "./calculatrice.module.scss";
 
-const CalculatriceView = ({ saveNumber, number, operate }) => {
-  const myArrayOfNumber = [
-    { key: "0", value: 0 },
-    { key: "1", value: 1 },
-    { key: "2", value: 2 },
-    { key: "3", value: 3 },
-    { key: "4", value: 4 },
-    { key: "5", value: 5 },
-    { key: "6", value: 6 },
-    { key: "7", value: 7 },
-    { key: "8", value: 8 },
-    { key: "9", value: 9 },
-  ];
-
-  const myArrayOfOperator = [
-    { key: "divide", value: "/" },
-    { key: "add", value: "+" },
-    { key: "substraction", value: "-" },
-    { key: "clear", value: "CE" },
-    { key: "egal", value: "=" },
-  ];
-
+const CalculatriceView = ({
+  saveNumber,
+  number,
+  operate,
+  myArrayOfOperator,
+  myArrayOfNumber,
+  expression,
+}) => {
+  console.log(expression);
   return (
     <>
       <div className={styles.calculator}>
@@ -32,11 +19,11 @@ const CalculatriceView = ({ saveNumber, number, operate }) => {
             return (
               <button
                 className={styles.number}
-                key={number.key}
+                key={number}
                 onClick={saveNumber}
-                value={number.key}
+                value={number}
               >
-                {number.value}
+                {number}
               </button>
             );
           })}
@@ -54,7 +41,10 @@ const CalculatriceView = ({ saveNumber, number, operate }) => {
               );
             })}
           </div>
-          <div className={styles.result}>{number}</div>
+
+          <div className={styles.result}>
+            {!expression ? number : expression}
+          </div>
         </div>
       </div>
     </>
